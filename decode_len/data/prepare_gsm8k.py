e0494e3052fa6bb41cf06e4a8304782fdcc8e65d
liangyuwang <ly.wang.2022@outlook.com>
2025-06-24 03:45:44 +0300

    add gsm8k

--- decode_len/data/prepare_gsm8k.py
+++ decode_len/data/prepare_gsm8k.py
@@ -1,11 +1,13 @@
 import os
 import json, argparse
 from tqdm import tqdm
-from datasets import load_dataset
+from datasets import load_dataset, concatenate_datasets
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 
 def main(args):
-    dataset = load_dataset("lmsys/lmsys-chat-1m", split=f"train[:{args.num_samples}]")
+    train_set = load_dataset(args.dataset_name, name="main", split="train")
+    test_set = load_dataset(args.dataset_name, name="main", split="test")
+    dataset = concatenate_datasets([train_set, test_set])
     
     # If needs, load LLM
     if args.use_llm:
@@ -18,15 +20,13 @@ def main(args):
     output = []
     for example in tqdm(dataset):
         try:
-            prompt = example["conversation"][0]["content"]
+            prompt = example["question"]
 
             if args.use_llm:
                 response = generator(prompt, max_new_tokens=args.max_new_tokens, do_sample=False)[0]["generated_text"]
                 response_only = response[len(prompt):].strip()
             else:
-                if len(example["conversation"]) < 2:
-                    continue  # skip no response
-                response_only = example["conversation"][1]["content"]
+                response_only = example["answer"]
 
             # Tokenize and slice response
             response_tokens = tokenizer.encode(response_only, add_special_tokens=False)
@@ -48,14 +48,14 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset_name", type=str, default="lmsys/lmsys-chat-1m")
+    parser.add_argument("--dataset_name", type=str, default="openai/gsm8k")
     parser.add_argument("--num_samples", type=str, default="100%")  # support "1%", or "1000"
     parser.add_argument("--use_llm", action="store_true", help="Use LLM to generate response or not")
     parser.add_argument("--model_id", type=str, default="Qwen/Qwen3-8B", help="LLM for generating response, only valid when use_llm=True")
     parser.add_argument("--tokenizer_id", type=str, default=None, help="tokenizer (default the same to model_id)")
     parser.add_argument("--max_new_tokens", type=int, default=1024)
     parser.add_argument("--device", type=int, default=0)
-    parser.add_argument("--output_path", type=str, default="exps/data/lmsys_bert_prompt_lengths.jsonl")
+    parser.add_argument("--output_path", type=str, default="exps/data/gsm8k_bert_prompt_lengths.jsonl")
     parser.add_argument("--k", type=int, default=0, help="Number of response tokens to append to prompt")
     args = parser.parse_args()
     if args.output_path is not None:
